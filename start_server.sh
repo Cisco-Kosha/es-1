12217c5c73a3a15dd9b840f6a85c1a313a2ae95b
repenno <rapenno@gmail.com>
2023-07-14 16:24:19 -0700

    Create start_server.sh

---
+++ start_server.sh
@@ -0,0 +1,5 @@
+docker network create elastic
+docker pull docker.elastic.co/elasticsearch/elasticsearch:8.8.2
+docker run --name es01 --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.8.2
+
+
