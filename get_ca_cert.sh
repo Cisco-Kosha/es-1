3d43f6120a98bac82bf297034eecd49bafce6b22
reinaldopenno <rapenno@gmail.com>
2023-07-16 14:11:11 -0700

    Script minor enhancements

--- get_ca_cert.sh
+++ get_ca_cert.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+rm http_ca.crt
 docker cp es01:/usr/share/elasticsearch/config/certs/http_ca.crt .
--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+docker rm -f es01
 docker network create elastic
 docker pull docker.elastic.co/elasticsearch/elasticsearch:8.8.2
 docker run --name es01 --net elastic -p 9200:9200 -it docker.elastic.co/elasticsearch/elasticsearch:8.8.2
