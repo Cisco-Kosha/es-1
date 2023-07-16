38bffe8a90139ebaf811b1f5f6a4f595a2ce3d9c
reinaldopenno <rapenno@gmail.com>
2023-07-16 13:05:13 -0700

    Minor update to start scripts

--- start_kib.sh
+++ start_kib.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+
+docker rm -f kib-01
 docker pull docker.elastic.co/kibana/kibana:8.8.2
 docker run --name kib-01 --net elastic -p 5601:5601 docker.elastic.co/kibana/kibana:8.8.2
